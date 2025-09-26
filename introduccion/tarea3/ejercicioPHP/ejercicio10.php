6b034ebbb7f488acfbbae918a457d676063f3f69
mackstm <thelewyntm@gmail.com>
2025-09-26 16:21:05 +0100

    feat: Se ha creado la primera parte del ejercicio 4

--- introduccion/tarea3/ejercicioPHP/ejercicio10.php
+++ introduccion/tarea3/ejercicioPHP/ejercicio10.php
@@ -1,4 +1,21 @@
 <?php
-    $fileName = "../outputPHP/datos.json";
-    var_dump(json_decode(file_get_contents($fileName)));
+$fileName = "../outputPHP/datos.json";
+if(!file_exists($fileName)){
+    $file=fopen($fileName, "w");
+    $datos = [
+        ["nombre" => "Ana", "edad" => 25],
+        ["nombre" => "Luis", "edad" => 30]
+    ];
+    fwrite($file, "[\n");
+    foreach ($datos as $persona) {
+        fwrite($file, json_encode($persona));
+        if ($persona !== end($datos)) {
+            fwrite($file, ",\n");
+        }
+    }
+    fwrite($file, "\n]");
+    fclose($file);
+}
+
+echo file_get_contents($fileName);
 ?>
---
+++ introduccion/tarea4/ejercicioPHP/ejercicio1.php
@@ -0,0 +1,35 @@
+<?php
+
+    $fileNameInput = "../inputPHP/ops.csv";
+    $fileNameOutput = "../outputPHP/resultado.csv";
+    if(file_exists($fileNameInput)){
+        
+        $contenidoString = file_get_contents($fileNameInput);
+        $arrLineas = explode("\n",$contenidoString);
+        foreach ($arrLineas as $lineas) {
+            
+            $fileOutput=fopen($fileNameOutput,"a");
+            $arr=explode(",",$lineas);
+            if($arr[2]=="suma"){
+                fwrite($fileOutput,$lineas.",".($arr[0]+$arr[1])."\n");
+            }
+            if($arr[2]=="resta"){
+                fwrite($fileOutput,$lineas.",".($arr[0]-$arr[1])."\n");
+            }
+            if($arr[2]=="producto"){
+                fwrite($fileOutput,$lineas.",".($arr[0]*$arr[1])."\n");
+            }
+            if($arr[2]=="division"){
+                if($arr[1]!=0){
+                    fwrite($fileOutput,$lineas.",".($arr[0]/$arr[1])."\n");
+                }else{
+                    fwrite($fileOutput,$lineas.",ERROR\n");
+                }
+            }
+            
+        }
+
+    }
+    
+    
+?>
