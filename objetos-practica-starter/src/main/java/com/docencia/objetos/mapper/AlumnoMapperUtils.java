93a3b537bd911c35a4a794e408bee94a545c8f5d
mackstm <IsmaelJos@github.com>
2025-11-14 16:18:01 +0000

    feat:

---
+++ objetos-practica-starter/src/main/java/com/docencia/objetos/mapper/AlumnoMapperUtils.java
@@ -0,0 +1,49 @@
+package com.docencia.objetos.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.docencia.objetos.domain.Alumno;
+import com.docencia.objetos.repo.jpa.AlumnoEntity;
+
+public class AlumnoMapperUtils {
+
+    /**
+     * funcion que transforma un AlumnoEntity a Alumno
+     * @param alumnoEntity
+     * @return
+     */
+    public static Alumno to(AlumnoEntity alumnoEntity){
+        if (alumnoEntity == null) {
+            return null;
+        }
+        Alumno alumno = new Alumno(alumnoEntity.getId(),alumnoEntity.getNombre(),alumnoEntity.getEmail(),alumnoEntity.getCiclo());
+        return alumno;
+    }
+    public static AlumnoEntity to(Alumno alumno){
+        if (alumno == null) {
+            return null;
+        }
+        return new AlumnoEntity(alumno.getId(),alumno.getNombre(),alumno.getEmail(),alumno.getCiclo());
+
+    }
+    public static List<Alumno> to(List<AlumnoEntity> alumnosEntity){
+        
+        List<Alumno> alumnos = new ArrayList<>();
+        if (alumnosEntity == null || alumnosEntity.isEmpty()) {
+            return alumnos;
+        }
+        for (AlumnoEntity alumnoEntity : alumnosEntity){
+            alumnos.add(to(alumnoEntity));
+        }
+        return alumnos;
+    }
+    public static Optional<Alumno> to(Optional<AlumnoEntity> alumnosEntity) {
+        Optional<Alumno> resultado = java.util.Optional.empty();
+        if (alumnosEntity == null || alumnosEntity.isEmpty()) {
+            return resultado;
+        }
+        return resultado.map(alumno->to(alumnosEntity).orElse(null));
+    }
+}
