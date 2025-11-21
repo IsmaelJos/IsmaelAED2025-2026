7b2e95f659d97df6347514fe67296ea9f55b38d9
mackstm <IsmaelJos@github.com>
2025-11-21 16:23:35 +0000

    feat:

---
+++ crud-mongo/src/main/java/com/docencia/personas/services/implementacion/PersonaService.java
@@ -0,0 +1,44 @@
+package com.docencia.personas.services.implementacion;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.docencia.personas.model.Persona;
+import com.docencia.personas.repository.PersonaRepository;
+import com.docencia.personas.services.IPersonaService;
+
+@Service
+public class PersonaService implements IPersonaService {
+
+    private final PersonaRepository personaRepository;
+
+    public PersonaService(PersonaRepository personaRepository) {
+        this.personaRepository = personaRepository;
+    }
+
+    @Override
+    public List<Persona> findAll() {
+        return personaRepository.findAll();
+    }
+
+    @Override
+    public Persona findByID(String id) {
+        // return personaRepository.findById(id);
+        return null;
+    }
+
+    @Override
+    public Persona save(Persona persona) {
+        return personaRepository.save(persona);
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        personaRepository.deleteById(id);
+        return true;
+    }
+
+
+
+}
